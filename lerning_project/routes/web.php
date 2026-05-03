fec23229a354930061f67229f3105332efefbc1b
Begadar <ramil.kustov@mail.ru>
2026-05-03 17:38:34 +0400

    added a call to the controller in the route 'my_page'

--- lerning_project/routes/web.php
+++ lerning_project/routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\MyPlaceController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/my_page', function () {
-    return 'this is my page';
-});
+Route::get('/my_page', [MyPlaceController::class, 'index']);
