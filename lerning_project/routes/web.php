a5480b625f32dac4edddaace2f7932c118fdb857
Begadar <ramil.kustov@mail.ru>
2026-05-05 10:03:35 +0400

    renamed 'MyPlaceController' to 'LessonController' and renamed route 'my_page' to 'lessons'

--- lerning_project/routes/web.php
+++ lerning_project/routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Http\Controllers\MyPlaceController;
+use App\Http\Controllers\LessonController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/my_page', [MyPlaceController::class, 'index']);
+Route::get('/lessons', [LessonController::class, 'index']);
